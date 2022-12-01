f6e58c3fb00cd6b5f188d601e2733d79a78ec33f
bodwood <bodiewood@hotmail.com>
2022-11-30 20:50:40 -0800

    add initial files with public void main and an empty dictionary

---
+++ PhoneBook.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+class PhoneBook{
+  public static Dictionary<string, string> phoneNumbers = new Dictionary<string, string>();
+
+
+static void Main()
+{
+  
+}
+
+}
