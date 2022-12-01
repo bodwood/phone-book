df1a3ad759b2256a47f35a5909a8cd01d6d4ebe6
bodwood <bodiewood@hotmail.com>
2022-11-30 21:07:33 -0800

    add new method to add a contact name and phone number to the phoneNumbers dictionary

--- PhoneBook.cs
+++ PhoneBook.cs
@@ -7,7 +7,27 @@ class PhoneBook{
 
 static void Main()
 {
+  Console.WriteLine("Would you like to enter a new contact to the dictionary? Type 'Y' for yes and 'N' for no");
+  string answer = Console.ReadLine();
+  if(answer == "Y" || answer == "y"){
+    addContact();
+  }
+  Console.WriteLine(phoneNumbers);
+
+}
+static void addContact(){
+  Console.WriteLine("Please enter their name: ");
+  string newName = Console.ReadLine();
+  Console.WriteLine("Please enter their phone number: ");
+  string newNumber = Console.ReadLine();
   
+  phoneNumbers.Add(newName, newNumber);
+}
+
+
+
+static void LookUpContact(){
+
 }
 
 }
