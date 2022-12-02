d9ab70792f1468a93e9aa6b2766dbc0d45f98eac
bodwood <bodiewood@hotmail.com>
2022-12-02 12:32:48 -0800

    add functionality to allow users to lookup more than one contact back-to-back

--- PhoneBook.cs
+++ PhoneBook.cs
@@ -5,47 +5,39 @@ class PhoneBook
 {
   public static Dictionary<string, string> phoneNumbers = new Dictionary<string, string>();
 
-
   static void Main()
   {
-    Console.WriteLine("\n|| Main Menu || \nPlease make your selection:\n[1] Add new contact\n[2] Lookup a contact new contact");
+    Console.WriteLine("\n|| Main Menu || \nPlease make your selection:\n[1] Add new contact\n[2] Lookup a contact");
     string answer = Console.ReadLine();
     if (answer == "1")
     {
+      Console.WriteLine("\nEnter new contact details below:");
       addContact();
     }
     else if (answer == "2")
     {
-      Console.WriteLine("\nWould you like to lookup a contact in the dictionary? Type 'Y' for yes and 'N' for no");
-      string lookUpAnswer = Console.ReadLine();
-      if (lookUpAnswer == "Y" || lookUpAnswer == "y")
-      {
-        LookUpContact();
-      }
-
+      LookUpContact();
     }
-
-
-
   }
-
 
   static void addContact()
   {
-    
-    Console.WriteLine("Please enter their name: ");
+    Console.WriteLine("Contact First Name: ");
     string newName = Console.ReadLine();
-    Console.WriteLine("Please enter their phone number: ");
+    Console.WriteLine("Contact Phone Number: ");
     string newNumber = Console.ReadLine();
 
     phoneNumbers.Add(newName, newNumber);
 
-    Console.WriteLine("\nWould you like to add another person to the dictionary? Type 'Y' for yes and 'N' for no");
+    Console.WriteLine("\nAdd another contact to the dictionary? \nType 'Y' for yes and 'N' for no");
     string anotherContact = Console.ReadLine();
-    if(anotherContact == "Y" || anotherContact == "y"){
+    if (anotherContact == "Y" || anotherContact == "y")
+    {
       addContact();
-    }else{
-    Main();
+    }
+    else
+    {
+      Main();
     }
   }
 
@@ -53,17 +45,26 @@ class PhoneBook
   {
     if (phoneNumbers.Count == 0)
     {
-      Console.WriteLine("\n!!!ERROR!!! Dictionary is empty Please add a contact to the dictionary before continuing.");
+      Console.WriteLine("\n!!!ERROR!!! Dictionary is empty. \nPlease add a contact to the dictionary before continuing.");
       Console.WriteLine("*Redirecting to Main Menu*. . .");
       Main();
     }
-    Console.WriteLine("Who would you like to look up in the dictionary?");
+    Console.WriteLine("\nPlease enter the contacts first name: ");
     string personInput = Console.ReadLine();
     string findPersonValue = phoneNumbers[personInput];
 
     Console.WriteLine(personInput + "\'s phone number is " + findPersonValue);
 
-    Main();
+    Console.WriteLine("\nLookup another contact in the dictionary? \nType 'Y' for yes and 'N' for no");
+    string anotherContact = Console.ReadLine();
+    if (anotherContact == "Y" || anotherContact == "y")
+    {
+      LookUpContact();
+    }
+    else
+    {
+      Main();
+    }
   }
 
 }
